e11e945a626d145006d1f561515c4ce6012e9db2
TheDevFreak <thedevfreak@gmail.com>
2020-08-14 11:00:56 +0100

    take integration from mr harcombe

---
+++ custom_components/metoffice/data.py
@@ -0,0 +1,111 @@
+"""Common Met Office Data class used by both sensor and entity."""
+
+import logging
+
+import datapoint
+
+from homeassistant.util import utcnow
+
+from .const import MODE_3HOURLY, MODE_DAILY
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class MetOfficeData:
+    """Get current and forecast data from Datapoint.
+
+    Please note that the 'datapoint' library is not asyncio-friendly, so some
+    calls have had to be wrapped with the standard hassio helper
+    async_add_executor_job.
+    """
+
+    def __init__(self, hass, api_key, latitude, longitude):
+        """Initialize the data object."""
+        self._hass = hass
+        self._datapoint = datapoint.connection(api_key=api_key)
+        self._site = None
+
+        # Public attributes
+        self.latitude = latitude
+        self.longitude = longitude
+
+        # Holds the current data from the Met Office
+        self.site_id = None
+        self.site_name = None
+        self.forecast_3hourly = None
+        self.forecast_daily = None
+        self.now_3hourly = None
+        self.now_daily = None
+
+    async def async_update_site(self):
+        """Async wrapper for getting the DataPoint site."""
+        return await self._hass.async_add_executor_job(self._update_site)
+
+    def _update_site(self):
+        """Determine the nearest DataPoint Site(s) to the held latitude/longitude."""
+        try:
+            new_site = self._datapoint.get_nearest_forecast_site(
+                latitude=self.latitude, longitude=self.longitude
+            )
+            if self._site is None or self._site.id != new_site.id:
+                self._site = new_site
+                self.forecast_3hourly = None
+                self.forecast_daily = None
+                self.now_3hourly = None
+                self.now_daily = None
+
+            self.site_id = self._site.id
+            self.site_name = self._site.name
+
+        except datapoint.exceptions.APIException as err:
+            _LOGGER.error("Received error from Met Office Datapoint: %s", err)
+            self._site = None
+            self.site_id = None
+            self.site_name = None
+            self.forecast_3hourly = None
+            self.forecast_daily = None
+            self.now_3hourly = None
+            self.now_daily = None
+
+        return self._site is not None
+
+    async def async_update(self):
+        """Async wrapper for update method."""
+        return await self._hass.async_add_executor_job(self._update)
+
+    def _update(self):
+        """Get the latest data from DataPoint."""
+        if self._site is None:
+            _LOGGER.error("No Met Office sites held, check logs for problems")
+            return
+
+        try:
+            forecast_3hourly = self._datapoint.get_forecast_for_site(
+                self._site.id, MODE_3HOURLY
+            )
+            forecast_daily = self._datapoint.get_forecast_for_site(
+                self._site.id, MODE_DAILY
+            )
+        except (ValueError, datapoint.exceptions.APIException) as err:
+            _LOGGER.error("Check Met Office connection: %s", err.args)
+            self.forecast_3hourly = None
+            self.forecast_daily = None
+            self.now_3hourly = None
+            self.now_daily = None
+        else:
+            self.now_3hourly = forecast_3hourly.now()
+            self.now_daily = forecast_daily.now()
+
+            time_now = utcnow()
+            self.forecast_3hourly = [
+                timestep
+                for day in forecast_3hourly.days
+                for timestep in day.timesteps
+                if timestep.date > time_now
+            ]
+            self.forecast_daily = [
+                timestep
+                for day in forecast_daily.days
+                for timestep in day.timesteps
+                if timestep.date > time_now
+            ]
--- custom_components/metoffice/weather.py
+++ custom_components/metoffice/weather.py
@@ -1,110 +1,101 @@
 """Support for UK Met Office weather service."""
-import logging
 
-import datapoint as dp
-import voluptuous as vol
+import logging
 
 from homeassistant.components.weather import (
     ATTR_FORECAST_CONDITION,
     ATTR_FORECAST_PRECIPITATION,
     ATTR_FORECAST_TEMP,
-    ATTR_FORECAST_TEMP_LOW,
     ATTR_FORECAST_TIME,
     ATTR_FORECAST_WIND_BEARING,
     ATTR_FORECAST_WIND_SPEED,
-    PLATFORM_SCHEMA,
     WeatherEntity,
 )
-from homeassistant.const import (
-    CONF_API_KEY,
-    CONF_LATITUDE,
-    CONF_LONGITUDE,
-    CONF_NAME,
-    TEMP_CELSIUS,
-)
-from homeassistant.helpers import config_validation as cv
+from homeassistant.const import LENGTH_KILOMETERS, TEMP_CELSIUS
+from homeassistant.core import callback
+from homeassistant.helpers.typing import ConfigType, HomeAssistantType
 
-from .sensor import ATTRIBUTION, CONDITION_CLASSES, MetOfficeCurrentData
+from .const import (
+    ATTRIBUTION,
+    CONDITION_CLASSES,
+    DEFAULT_NAME,
+    DOMAIN,
+    METOFFICE_COORDINATOR,
+    METOFFICE_DATA,
+    METOFFICE_NAME,
+    MODE_3HOURLY_LABEL,
+    MODE_DAILY,
+    MODE_DAILY_LABEL,
+    VISIBILITY_CLASSES,
+    VISIBILITY_DISTANCE_CLASSES,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = "Met Office"
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-    {
-        vol.Required(CONF_API_KEY): cv.string,
-        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-        vol.Inclusive(
-            CONF_LATITUDE, "coordinates", "Latitude and longitude must exist together"
-        ): cv.latitude,
-        vol.Inclusive(
-            CONF_LONGITUDE, "coordinates", "Latitude and longitude must exist together"
-        ): cv.longitude,
-    }
-)
+async def async_setup_entry(
+    hass: HomeAssistantType, entry: ConfigType, async_add_entities
+) -> None:
+    """Set up the Met Office weather sensor platform."""
+    hass_data = hass.data[DOMAIN][entry.entry_id]
 
-
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the Met Office weather platform."""
-    name = config.get(CONF_NAME)
-    datapoint = dp.connection(api_key=config.get(CONF_API_KEY))
-
-    latitude = config.get(CONF_LATITUDE, hass.config.latitude)
-    longitude = config.get(CONF_LONGITUDE, hass.config.longitude)
-
-    if None in (latitude, longitude):
-        _LOGGER.error("Latitude or longitude not set in Home Assistant config")
-        return
-
-    try:
-        site = datapoint.get_nearest_site(latitude=latitude, longitude=longitude)
-    except dp.exceptions.APIException as err:
-        _LOGGER.error("Received error from Met Office Datapoint: %s", err)
-        return
-
-    if not site:
-        _LOGGER.error("Unable to get nearest Met Office forecast site")
-        return
-
-    data = MetOfficeCurrentData(hass, datapoint, site)
-    try:
-        data.update()
-    except (ValueError, dp.exceptions.APIException) as err:
-        _LOGGER.error("Received error from Met Office Datapoint: %s", err)
-        return
-
-    add_entities([MetOfficeWeather(site, data, name)], True)
+    async_add_entities(
+        [
+            MetOfficeWeather(entry.data, hass_data, True),
+            MetOfficeWeather(entry.data, hass_data, False),
+        ],
+        False,
+    )
 
 
 class MetOfficeWeather(WeatherEntity):
     """Implementation of a Met Office weather condition."""
 
-    def __init__(self, site, data, name):
-        """Initialise the platform with a data instance and site."""
-        self._name = name
-        self.data = data
-        self.site = site
+    def __init__(self, entry_data, hass_data, use_3hourly):
+        """Initialise the platform with a data instance."""
+        self._data = hass_data[METOFFICE_DATA]
+        self._coordinator = hass_data[METOFFICE_COORDINATOR]
 
-    def update(self):
-        """Update current conditions."""
-        self.data.update()
+        self._name = f"{DEFAULT_NAME} {hass_data[METOFFICE_NAME]} {MODE_3HOURLY_LABEL if use_3hourly else MODE_DAILY_LABEL}"
+        self._unique_id = f"{self._data.latitude}_{self._data.longitude}"
+        if not use_3hourly:
+            self._unique_id = f"{self._unique_id}_{MODE_DAILY}"
+
+        self.use_3hourly = use_3hourly
+        self.metoffice_now = None
+        self.metoffice_forecast = None
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return f"{self._name} {self.site.name}"
+        return self._name
+
+    @property
+    def unique_id(self):
+        """Return the unique of the sensor."""
+        return self._unique_id
 
     @property
     def condition(self):
         """Return the current condition."""
-        return [
-            k for k, v in CONDITION_CLASSES.items() if self.data.data.weather.value in v
-        ][0]
+        return (
+            [
+                k
+                for k, v in CONDITION_CLASSES.items()
+                if self.metoffice_now.weather.value in v
+            ][0]
+            if self.metoffice_now
+            else None
+        )
 
     @property
     def temperature(self):
         """Return the platform temperature."""
-        return self.data.data.temperature.value
+        return (
+            self.metoffice_now.temperature.value
+            if self.metoffice_now and self.metoffice_now.temperature
+            else None
+        )
 
     @property
     def temperature_unit(self):
@@ -112,56 +103,116 @@ class MetOfficeWeather(WeatherEntity):
         return TEMP_CELSIUS
 
     @property
+    def visibility(self):
+        """Return the platform visibility."""
+        _visibility = None
+        if hasattr(self.metoffice_now, "visibility"):
+            _visibility = f"{VISIBILITY_CLASSES.get(self.metoffice_now.visibility.value)} ({VISIBILITY_DISTANCE_CLASSES.get(self.metoffice_now.visibility.value)})"
+        return _visibility
+
+    @property
+    def visibility_unit(self):
+        """Return the unit of measurement."""
+        return LENGTH_KILOMETERS
+
+    @property
     def pressure(self):
         """Return the mean sea-level pressure."""
-        return None
+        return (
+            self.metoffice_now.pressure.value
+            if self.metoffice_now and self.metoffice_now.pressure
+            else None
+        )
 
     @property
     def humidity(self):
         """Return the relative humidity."""
-        return self.data.data.humidity.value
+        return (
+            self.metoffice_now.humidity.value
+            if self.metoffice_now and self.metoffice_now.humidity
+            else None
+        )
 
     @property
     def wind_speed(self):
         """Return the wind speed."""
-        return self.data.data.wind_speed.value
+        return (
+            self.metoffice_now.wind_speed.value
+            if self.metoffice_now and self.metoffice_now.wind_speed
+            else None
+        )
 
     @property
     def wind_bearing(self):
         """Return the wind bearing."""
-        return self.data.data.wind_direction.value
+        return (
+            self.metoffice_now.wind_direction.value
+            if self.metoffice_now and self.metoffice_now.wind_direction
+            else None
+        )
+
+    @property
+    def forecast(self):
+        """Return the forecast array."""
+        data = [
+            {
+                ATTR_FORECAST_CONDITION: [
+                    k
+                    for k, v in CONDITION_CLASSES.items()
+                    if timestep.weather.value in v
+                ][0]
+                if timestep.weather
+                else None,
+                ATTR_FORECAST_PRECIPITATION: timestep.precipitation.value
+                if timestep.precipitation
+                else None,
+                ATTR_FORECAST_TEMP: timestep.temperature.value
+                if timestep.temperature
+                else None,
+                ATTR_FORECAST_TIME: timestep.date,
+                ATTR_FORECAST_WIND_BEARING: timestep.wind_direction.value
+                if timestep.wind_direction
+                else None,
+                ATTR_FORECAST_WIND_SPEED: timestep.wind_speed.value
+                if timestep.wind_speed
+                else None,
+            }
+            for timestep in self.metoffice_forecast
+        ]
+
+        return data
 
     @property
     def attribution(self):
         """Return the attribution."""
         return ATTRIBUTION
-    
+
+    async def async_added_to_hass(self) -> None:
+        """Set up a listener and load data."""
+        self.async_on_remove(
+            self._coordinator.async_add_listener(self._update_callback)
+        )
+        self._update_callback()
+
+    @callback
+    def _update_callback(self) -> None:
+        """Load data from integration."""
+        self.metoffice_now = (
+            self._data.now_3hourly if self.use_3hourly else self._data.now_daily
+        )
+        self.metoffice_forecast = (
+            self._data.forecast_3hourly
+            if self.use_3hourly
+            else self._data.forecast_daily
+        )
+        self.async_write_ha_state()
+
     @property
-    def forecast(self):
-        """Returns forecast array"""
-        conditions = {
+    def should_poll(self) -> bool:
+        """Entities do not individually poll."""
+        return False
 
-        }
-        data = []
-        def get_daily_min_max_temp(day):
-            temperatures = []
-            for step in day.timesteps:
-                temperatures.append(step.temperature.value)
-            return min(temperatures), max(temperatures)
-        def get_day_condition(day):
-            for k, v in CONDITION_CLASSES.items():
-                if day.timesteps[4].weather.value in v:
-                    return(k)
-
-
-        
-        for day in self.data.d_data.days:
-            min_temp, max_temp = get_daily_min_max_temp(day)
-            day = {
-                ATTR_FORECAST_TIME: day.timesteps[0].date.isoformat(),
-                ATTR_FORECAST_TEMP: max_temp,
-                ATTR_FORECAST_TEMP_LOW: min_temp,
-                ATTR_FORECAST_CONDITION: get_day_condition(day)
-            }
-            data.append(day)
-        return data
+    @property
+    def available(self):
+        """Return if state is available."""
+        return self.metoffice_now is not None and self.metoffice_forecast is not None
